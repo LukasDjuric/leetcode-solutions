cbb17ca4063ab82fc439914401e400c98b769d35
Luka Đurić <123109587+LukasDjuric@users.noreply.github.com>
2026-06-08 11:59:50 +0200

    Create leetcode2161.c
    Partition Array According to Given Pivot
    
    The array is partitioned by collecting elements smaller and greater than the pivot into separate temporary arrays while counting occurrences of the pivot. The final array is then reconstructed in three sections: values smaller than the pivot, pivot values, and values greater than the pivot. Since elements are processed from left to right, their original relative order is preserved. Complexity is O(n) time and O(n) extra space.

---
+++ leetcode2161.c
@@ -0,0 +1,34 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
+
+    int* ptrG = calloc(numsSize, sizeof(int));
+    int* ptrL = calloc(numsSize, sizeof(int));
+    int* ptr = calloc(numsSize, sizeof(int));
+    *returnSize = numsSize;
+    int countG = 0, countL = 0, i, countE = 0;
+    for(i = 0; i < numsSize; i++){
+        if(nums[i] == pivot){
+            countE++;
+        }else if(nums[i] > pivot){
+            ptrG[countG] = nums[i];
+            countG++;
+        }else{
+            ptrL[countL] = nums[i];
+            countL++;
+        }
+    }
+    for(i = 0; i < countL; i++){
+        ptr[i] = ptrL[i];
+    }
+    for(i = 0; i < countE; i++){
+        ptr[i + countL] = pivot;
+    }
+    for(i = 0; i < countG; i++){
+        ptr[i + countL + countE] = ptrG[i];
+    }
+
+    return ptr;
+
+}
